3f7912dac31b64384dd68038a96bd2796911ca09
Rafael Mariano <rhmariano@gmail.com>
2023-07-02 20:47:22 -0300

    Update DashboardVinhos.py

--- DashboardVinhos.py
+++ DashboardVinhos.py
@@ -88,7 +88,7 @@ df_normalized['date'] = pd.to_datetime(df_normalized['date_string'])
 df_2021 = df_normalized[df_normalized['year'] == '2021']
 
 # Filtering Last 15 years
-df_wine_last_15_year = df_normalized[df_normalized['date'] > '2007-01-01']
+df_wine_last_15_year = df_normalized[df_normalized['date'] >= '2007-01-01']
 
 # Removing countries that didn't buy in the last 15y 
 df_total_liters_per_country = df_wine_last_15_year[['country_destination','liters']].groupby('country_destination').sum()
